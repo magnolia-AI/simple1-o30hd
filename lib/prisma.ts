594d33887021da8d6cc160ee7e63940088121f29
antonmagnus <antma062@student.liu.se>
2025-07-08 15:03:29 +0200

    Update dependencies and enhance Prisma configuration for edge environments

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,25 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import dotenv from 'dotenv'
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
 
-dotenv.config()
-const connectionString = `${process.env.DATABASE_URL}`
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
 
-const adapter = new PrismaNeon({ connectionString })
-const prisma = new PrismaClient({ adapter })
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+neonConfig.poolQueryViaFetch = true
+
+// Type definitions
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
